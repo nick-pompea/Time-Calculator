1488a4d664a53976eae22a4aa0be66e5250f8bb8
Nicholas Pompea <nick.pompea@gmail,com>
2022-11-10 11:53:31 -0700

    Initial Commit

---
+++ Time Calculator/Time_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Time_CalculatorApp.swift
+//  Time Calculator
+//
+//  Created by Nicholas Pompea on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Time_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
